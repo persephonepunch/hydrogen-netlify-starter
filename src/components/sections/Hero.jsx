e6102507792f595a717a5e524e0cb7d3f5f0944e
yoonsun lee <yoonsunlee150@gmail.com>
2022-07-19 17:16:48 -0400

    footjoy placeholders

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -28,13 +28,56 @@ export function Hero({
       >
             <div className="absolute inset-0 grid flex-grow grid-flow-col pointer-events-none auto-cols-fr -z-10 content-stretch overflow-clip">
 
-            <iframe name="footjoy" className="y-iframe" src="https://footjoy2.netlify.app/"></iframe>
-            </div>
+          <iframe name="footjoy" className="y-iframe" src="https://footjoy2.netlify.app/"></iframe>
+          </div>
     
        
-
+<div className="absolute inset-0 grid flex-grow grid-flow-col pointer-events-none auto-cols-fr -z-10 content-stretch overflow-clip">
+          {spread?.reference && (
+            <div className="">
+              <SpreadMedia
+                scale={2}
+                sizes={
+                  spreadSecondary?.reference
+                    ? '(min-width: 80em) 700px, (min-width: 48em) 450px, 500px'
+                    : '(min-width: 80em) 1400px, (min-width: 48em) 900px, 500px'
+                }
+                widths={
+                  spreadSecondary?.reference
+                    ? [500, 450, 700]
+                    : [500, 900, 1400]
+                }
+                width={spreadSecondary?.reference ? 375 : 750}
+                data={spread.reference}
+                loading={loading}
+              />
+            </div>
+          )}
+          {spreadSecondary?.reference && (
+            <div className="hidden md:block">
+              <SpreadMedia
+                sizes="(min-width: 80em) 700, (min-width: 48em) 450, 500"
+                widths={[450, 700]}
+                width={375}
+                data={spreadSecondary.reference}
+              />
+            </div>
+          )}
+        </div>
    
-       
+        <div className="flex flex-col items-baseline justify-between gap-4 px-6 py-8 sm:px-8 md:px-12 bg-gradient-to-t dark:from-contrast/60 dark:text-primary from-primary/60 text-contrast">
+          {heading?.value && (
+            <Heading format as="h2" size="display" className="max-w-md">
+              {heading.value}
+            </Heading>
+          )}
+          {byline?.value && (
+            <Text format width="narrow" as="p" size="lead">
+              {byline.value}
+            </Text>
+          )}
+          {cta?.value && <Text size="lead">{cta.value}</Text>}
+        </div>
       </section>
     </Link>
   );
